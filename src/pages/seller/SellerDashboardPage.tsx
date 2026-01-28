1f29ce85538377317c6dad32123cb67ec1f99829
Mwvndva <111277645+Mwvndva@users.noreply.github.com>
2026-01-28 14:17:51 +0300

    Phase 2: Migrate SellerLayout to BaseDashboardLayout - complete

--- src/pages/seller/SellerDashboardPage.tsx
+++ src/pages/seller/SellerDashboardPage.tsx
@@ -1,10 +1,5 @@
 import SellerDashboard from '@/components/seller/SellerDashboard';
-import { SellerLayout } from '@/components/layout/SellerLayout';
 
 export default function SellerDashboardPage() {
-  return (
-    <SellerLayout>
-      <SellerDashboard />
-    </SellerLayout>
-  );
+  return <SellerDashboard />;
 }
