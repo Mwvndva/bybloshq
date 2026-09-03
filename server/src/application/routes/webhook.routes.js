2d48f1904d795bbaa48f293aa81ee758c3718def
Byblos Developer <developer@bybloshq.com>
2026-09-04 01:59:06 +0300

    revert(webhook): restore exact '/paystack' route path

--- server/src/application/routes/webhook.routes.js
+++ server/src/application/routes/webhook.routes.js
@@ -5,7 +5,7 @@ import { requirePaystackWebhookHmac, verifyPaystackWebhook, webhookRateLimiter }
 const router = express.Router();
 
 router.post(
-    ['/paystack', '/payst'],
+    '/paystack',
     verifyPaystackWebhook,
     webhookRateLimiter,
     requirePaystackWebhookHmac,
